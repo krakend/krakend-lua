c0cfcbb33afd5f084c16b9aac6eaae34c9480a2e
kpacha <kpacha666@gmail.com>
2019-05-03 17:34:56 +0200

    tests for the router mws added

---
+++ router/gin/lua_test.go
@@ -0,0 +1,67 @@
+package gin
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/devopsfaith/krakend-lua/router"
+	"github.com/devopsfaith/krakend/config"
+	"github.com/devopsfaith/krakend/logging"
+	"github.com/devopsfaith/krakend/proxy"
+	"github.com/gin-gonic/gin"
+)
+
+func TestProxyFactory(t *testing.T) {
+	cfg := &config.EndpointConfig{
+		Endpoint: "/",
+		ExtraConfig: config.ExtraConfig{
+			router.Namespace: map[string]interface{}{
+				"sources": []interface{}{
+					"../../lua/factorial.lua",
+				},
+
+				"pre": `local req = ctx.load()
+		req:method("POST")
+		req:params("foo", "some_new_value")
+		req:headers("Accept", "application/xml")
+		req:url(req:url() .. "&more=true")`,
+			},
+		},
+	}
+
+	hf := func(_ *config.EndpointConfig, _ proxy.Proxy) gin.HandlerFunc {
+		return func(c *gin.Context) {
+			if URL := c.Request.URL.String(); URL != "/some-path/42?id=1&more=true" {
+				t.Errorf("unexpected URL: %s", URL)
+			}
+			if accept := c.Request.Header.Get("Accept"); accept != "application/xml" {
+				t.Errorf("unexpected accept header: %s", accept)
+			}
+			if "POST" != c.Request.Method {
+				t.Errorf("unexpected method: %s", c.Request.Method)
+			}
+			if foo := c.Param("foo"); foo != "some_new_value" {
+				t.Errorf("unexpected param foo: %s", foo)
+			}
+			if id := c.Param("id"); id != "42" {
+				t.Errorf("unexpected param id: %s", id)
+			}
+		}
+	}
+	handler := HandlerFactory(logging.NoOp, hf)(cfg, proxy.NoopProxy)
+
+	engine := gin.New()
+	engine.GET("/some-path/:id", handler)
+
+	req, _ := http.NewRequest("GET", "/some-path/42?id=1", nil)
+	req.Header.Set("Accept", "application/json")
+	w := httptest.NewRecorder()
+
+	engine.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("unexpected status code %d", w.Code)
+		return
+	}
+}
---
+++ router/mux/lua_test.go
@@ -0,0 +1,65 @@
+package mux
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/devopsfaith/krakend-lua/router"
+	"github.com/devopsfaith/krakend/config"
+	"github.com/devopsfaith/krakend/logging"
+	"github.com/devopsfaith/krakend/proxy"
+)
+
+func TestProxyFactory(t *testing.T) {
+	cfg := &config.EndpointConfig{
+		Endpoint: "/",
+		ExtraConfig: config.ExtraConfig{
+			router.Namespace: map[string]interface{}{
+				"sources": []interface{}{
+					"../../lua/factorial.lua",
+				},
+
+				"pre": `local req = ctx.load()
+		req:method("POST")
+		req:params("foo", "some_new_value")
+		req:headers("Accept", "application/xml")
+		req:url(req:url() .. "&more=true")`,
+			},
+		},
+	}
+
+	hf := func(_ *config.EndpointConfig, _ proxy.Proxy) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			if URL := r.URL.String(); URL != "/some-path/42?id=1&more=true" {
+				t.Errorf("unexpected URL: %s", URL)
+			}
+			if accept := r.Header.Get("Accept"); accept != "application/xml" {
+				t.Errorf("unexpected accept header: %s", accept)
+			}
+			if "POST" != r.Method {
+				t.Errorf("unexpected method: %s", r.Method)
+			}
+			// if foo := c.Param("foo"); foo != "some_new_value" {
+			// 	t.Errorf("unexpected param foo: %s", foo)
+			// }
+			// if id := c.Param("id"); id != "42" {
+			// 	t.Errorf("unexpected param id: %s", id)
+			// }
+		}
+	}
+	handler := HandlerFactory(logging.NoOp, hf, func(_ *http.Request) map[string]string {
+		return map[string]string{}
+	})(cfg, proxy.NoopProxy)
+
+	req, _ := http.NewRequest("GET", "/some-path/42?id=1", nil)
+	req.Header.Set("Accept", "application/json")
+	w := httptest.NewRecorder()
+
+	handler(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("unexpected status code %d", w.Code)
+		return
+	}
+}
